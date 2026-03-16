c0274846221b2583fbfe1810fa1c023e919efcc3
Devashish-Pisal <dpisal85@gmail.com>
2026-03-16 19:48:29 +0100

    central path configuartion file

---
+++ path_config.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).parent
+TRAIN_DATASET_PATH = PROJECT_ROOT / "data" / "raw" / "SROIE2019" / "train"
+TEST_DATASET_PATH = PROJECT_ROOT / "data" / "raw" / "SROIE2019" / "test"
+
+
+
