9a1ddf8104cea9e22b02b8b77b22db5485187e67
Devashish-Pisal <dpisal85@gmail.com>
2026-03-16 21:08:07 +0100

    add path for processed data

--- path_config.py
+++ path_config.py
@@ -4,6 +4,6 @@ from pathlib import Path
 PROJECT_ROOT = Path(__file__).parent
 TRAIN_DATASET_PATH = PROJECT_ROOT / "data" / "raw" / "SROIE2019" / "train"
 TEST_DATASET_PATH = PROJECT_ROOT / "data" / "raw" / "SROIE2019" / "test"
-
+PROCESSED_DATA_PATH = PROJECT_ROOT / "data" / "processed"
 
 
