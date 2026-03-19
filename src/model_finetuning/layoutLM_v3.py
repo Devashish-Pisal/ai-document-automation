67031fe146a854439ef88a93a5e09ee3b3091021
Devashish-Pisal <dpisal85@gmail.com>
2026-03-19 20:13:18 +0100

    save & load the preprocessed dataset

--- src/model_finetuning/layoutLM_v3.py
+++ src/model_finetuning/layoutLM_v3.py
@@ -2,6 +2,13 @@ from train_dataset_prep import TRAIN_DATA
 from test_dataset_prep import TEST_DATA
 from transformers import LayoutLMv3Processor, LayoutLMv3ForTokenClassification
 from dataset_util import preprocess
+from datasets import Dataset
+from path_config import PROCESSED_DATA_PATH
+from loguru import logger
+import os
+import torch
+
+
 
 # Collect unique labels from the datasets, so that we can assign them numbers (ids)
 unique_labels = set()
@@ -20,8 +27,52 @@ processor = LayoutLMv3Processor.from_pretrained( "microsoft/layoutlmv3-base", ap
 # Load base model
 model = LayoutLMv3ForTokenClassification.from_pretrained("microsoft/layoutlmv3-base", num_labels=len(unique_labels), id2label=id2label, label2id=label2id)
 
-# Preprocess both datasets for the training and testing
-ENCODED_TRAIN_DATASET = {}
-for sample in TRAIN_DATA:
-    encodings = preprocess(sample, label2id, processor)
+ENCODED_TRAIN_DATASET = None
+ENCODED_TEST_DATASET = None
+encoded_train_dataset_path = PROCESSED_DATA_PATH / 'ENCODED_TRAIN_DATASET.pt'
+encoded_test_dataset_path = PROCESSED_DATA_PATH / 'ENCODED_TEST_DATASET.pt'
+if os.path.exists(encoded_train_dataset_path) and  os.path.exists(encoded_test_dataset_path):
+    ENCODED_TRAIN_DATASET = torch.load(encoded_train_dataset_path)
+    logger.info("ENCODED_TRAIN_DATASET.pt loaded!")
+    ENCODED_TEST_DATASET = torch.load(encoded_test_dataset_path)
+    logger.info("ENCODED_TRAIN_DATASET.pt loaded!")
+else:
+    # Preprocess training dataset
+    ENCODED_TRAIN_DATASET = {'input_ids': [], 'attention_mask': [], 'bbox': [], 'labels': [], 'pixel_values': []}
+    for sample in TRAIN_DATA:
+        encodings = preprocess(sample, label2id, processor)
+        ENCODED_TRAIN_DATASET['input_ids'].append(encodings['input_ids'])
+        ENCODED_TRAIN_DATASET['attention_mask'].append(encodings['attention_mask'])
+        ENCODED_TRAIN_DATASET['bbox'].append(encodings['bbox'])
+        ENCODED_TRAIN_DATASET['labels'].append(encodings['labels'])
+        ENCODED_TRAIN_DATASET['pixel_values'].append(encodings['pixel_values'])
+    torch.save(ENCODED_TRAIN_DATASET, encoded_train_dataset_path)
+    logger.success(f"ENCODED_TRAIN_DATASET.pt successfully created!")
+
+    # Preprocess testing dataset
+    ENCODED_TEST_DATASET = {'input_ids': [], 'attention_mask': [], 'bbox': [], 'labels': [], 'pixel_values': []}
+    for sample in TEST_DATA:
+        encodings = preprocess(sample, label2id, processor)
+        ENCODED_TEST_DATASET['input_ids'].append(encodings['input_ids'])
+        ENCODED_TEST_DATASET['attention_mask'].append(encodings['attention_mask'])
+        ENCODED_TEST_DATASET['bbox'].append(encodings['bbox'])
+        ENCODED_TEST_DATASET['labels'].append(encodings['labels'])
+        ENCODED_TEST_DATASET['pixel_values'].append(encodings['pixel_values'])
+    torch.save(ENCODED_TEST_DATASET, encoded_test_dataset_path)
+    logger.success(f"ENCODED_TEST_DATASET.pt successfully created!")
+
+ENCODED_TRAIN_DATASET = Dataset.from_dict(ENCODED_TRAIN_DATASET)
+ENCODED_TEST_DATASET = Dataset.from_dict(ENCODED_TEST_DATASET)
+
+# Training & Testing Dataset
+'''
+Dataset({
+    features: ['input_ids', 'attention_mask', 'bbox', 'labels', 'pixel_values'],
+    num_rows: 626
+})
+Dataset({
+    features: ['input_ids', 'attention_mask', 'bbox', 'labels', 'pixel_values'],
+    num_rows: 345
+})
+'''
 
