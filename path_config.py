e9864fba3ebbc25faae3d211bb96bb66b453c430
Devashish-Pisal <dpisal85@gmail.com>
2026-05-16 22:36:39 +0200

    rewrite paths with 'data folder' path

--- path_config.py
+++ path_config.py
@@ -1,8 +1,11 @@
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).parent
-TRAIN_DATASET_PATH = PROJECT_ROOT / "data" / "raw" / "SROIE2019" / "train"
-TEST_DATASET_PATH = PROJECT_ROOT / "data" / "raw" / "SROIE2019" / "test"
-PROCESSED_DATA_PATH = PROJECT_ROOT / "data" / "processed"
-MODEL_DATA_PATH = PROJECT_ROOT / "model_data"
 
+DATA_DIR = PROJECT_ROOT / "data"
+TRAIN_DATASET_PATH = DATA_DIR / "raw" / "SROIE2019" / "train"
+TEST_DATASET_PATH = DATA_DIR / "raw" / "SROIE2019" / "test"
+PROCESSED_DATA_PATH = DATA_DIR / "processed"
+PROMPTS_DIR = DATA_DIR / "prompts"
+
+MODEL_DATA_PATH = PROJECT_ROOT / "model_data"
