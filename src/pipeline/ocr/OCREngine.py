7094327677f10647703f0cbbcc02bad0abbc6a1b
Devashish-Pisal <dpisal85@gmail.com>
2026-06-23 18:28:18 +0200

    compute complete ocr data, not only text

--- src/pipeline/ocr/OCREngine.py
+++ src/pipeline/ocr/OCREngine.py
@@ -4,15 +4,15 @@ import pytesseract
 from PIL import Image
 from pathlib import Path
 from config import config
+from loguru import logger
 from path_config import TEMP_FILES_DIR
 from pdf2image import convert_from_path
-from concurrent.futures import ThreadPoolExecutor
 
-ocr_pool = ThreadPoolExecutor(max_workers=config["max_workers"])
 
 class OCREngine:
-    def __init__(self, file_paths:list[Path]):
+    def __init__(self, file_paths:list[Path], thread_pool_executor):
         self.files_to_process = file_paths
+        self.thread_pool_executor = thread_pool_executor
         self.convert_pdfs_to_images()
 
     def convert_pdfs_to_images(self):
@@ -33,7 +33,7 @@ class OCREngine:
     async def perform_ocr(self):
         loop = asyncio.get_running_loop()
         tasks = [
-            loop.run_in_executor(ocr_pool, self.perform_pytesseract_ocr, file_path)
+            loop.run_in_executor(self.thread_pool_executor, self.perform_pytesseract_ocr, file_path)
             for file_path in self.files_to_process
         ]
         result = await asyncio.gather(*tasks)
@@ -42,11 +42,14 @@ class OCREngine:
 
     @staticmethod
     def perform_pytesseract_ocr(file_path):
-        img = Image.open(file_path)
-        text =  pytesseract.image_to_string(img)
+        ocr = None
+        with Image.open(file_path) as img:
+            ocr =  pytesseract.image_to_data(img, output_type=pytesseract.Output.DICT)
+        if not ocr:
+            logger.error(f"OCR data for file {file_path} is 'None'") # Check for None ocr values when doing model inference and skip those images
         return {
             "file": file_path,
-            "text": text,
+            "ocr": ocr,
         }
 
 
